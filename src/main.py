7fec287c63041516b0d1eb91cba674480109505b
Jorge Saenz <saenzdiazj0@gmail.com>
2024-04-14 14:42:05 -0500

    feat(bot): :sparkles: Add play, pause, resume and skip commands

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import discord.context_managers
 from discord.ext import commands
 from discord.utils import get
 
+from pytube import YouTube
+import moviepy.editor as mp
+
 from dotenv import load_dotenv
 import os
 
@@ -12,8 +15,9 @@ TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.voice_states = True
 
-bot = commands.Bot(intents=intents, description="Bot Music Sam",
+bot = commands.Bot(intents=intents, description="Bot Music",
                    command_prefix=commands.when_mentioned_or("!d "))
 
 
@@ -48,6 +52,104 @@ async def off(ctx):
     channel = ctx.message.author.voice.channel
     voice = get(bot.voice_clients, guild=ctx.guild)
     await voice.disconnect()
+
+
+def download_video(url: str) -> str:
+    yt = YouTube(url)
+    video = yt.streams.filter(progressive=True).order_by(
+        "resolution").asc().first()
+
+    try:
+        message = video.download(output_path="downloads/")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        return message
+
+
+def audio_extraction(path: str):
+    video_clip = mp.AudioFileClip(path)
+    video_clip.write_audiofile("downloads/song.mp3")
+
+
+def get_song(url: str) -> str:
+    path = download_video(url)
+    print(f"Video downloaded on '{path}'")
+    file_name = os.path.basename(path)
+    name = os.path.splitext(file_name)[0]
+
+    audio_extraction(path)
+    os.remove(path)
+
+    return name
+
+
+@bot.command(pass_context=True)
+async def play(ctx, url: str):
+    songPlaying = os.path.isfile("../downloads/song.mp3")
+
+    try:
+        if songPlaying:
+            os.remove("../downloads/song.mp3")
+            print("Song was removed")
+    except PermissionError:
+        print("A song is playing")
+        await ctx.send("E: A song is playing")
+        return
+
+    await ctx.send("Ok!!")
+
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    song_name = get_song(url)
+
+    voice.play(discord.FFmpegPCMAudio("downloads/song.mp3"),
+               after=lambda e: print("Finish"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.04
+
+    await ctx.send(f"Playing: {song_name}")
+
+
+@bot.command(pass_context=True)
+async def pause(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if not (voice and voice.is_playing()):
+        print("E: Music is not playing")
+        await ctx.send("Music is not playing")
+        return
+
+    print(f"Music was paused by {ctx.author}")
+    voice.pause()
+    await ctx.send(f"Music was paused by {ctx.author}")
+
+
+@bot.command(pass_context=True)
+async def resume(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if not (voice and voice.is_paused()):
+        print("E: Music is not paused")
+        await ctx.send("Music is not paused")
+        return
+
+    print("Music was resume")
+    voice.resume()
+    await ctx.send("Music was resume")
+
+
+@bot.command(pass_context=True)
+async def skip(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if not (voice and voice.is_playing()):
+        print("E: Music is not playing")
+        await ctx.send("Music is not playing")
+        return
+
+    print(f"Music was skipped by {ctx.author}")
+    voice.stop()
+    await ctx.send(f"Music was skipped by {ctx.author}")
 
 
 bot.run(TOKEN)
